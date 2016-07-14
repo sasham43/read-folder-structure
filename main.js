93cc092a1ef2bd881e414f50eaa6f296396aeb4e
sasham43 <sashasemail@gmail.com>
2016-07-14 09:34:01 -0500

    Read .gitignore contents

--- main.js
+++ main.js
@@ -8,6 +8,15 @@ var fileStructure = {
 
 var currentPath = '/Users/sashakramer/workspace/read-file-structure';  // replace this with user input
 
+var gitIgnorePath = currentPath + '/.gitignore';
+var gitIgnoreContents = [];
+
+// read .gitignore to ignore its contents in the file structure
+if(fs.lstatSync(gitIgnorePath).isFile()){
+  var gitIgnoreString = fs.readFileSync(gitIgnorePath, 'utf-8');
+  gitIgnoreContents = gitIgnoreString.split('\n');
+}
+
 readFolder(currentPath, fileStructure);
 
 console.log('fileStructure', fileStructure);
